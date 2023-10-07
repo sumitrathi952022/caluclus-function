2fc931e1698d4db3f65fd687af871e65a0492795
osboxes.org <sumitrathi952022@gmail.com>
2023-10-07 00:49:44 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+	return 'Hello %s!' % name
+
+@app.route('/<value>')
+def hello_value(value):
+	print(value)
+	return 'Value printed on Command Line'
+
+@app.route('/mult/<number>')
+def hello_number(number):
+	print(number)
+	mult = 5 * int(number)
+	print(mult)
+	return str(mult)
+
+if __name__ == '__main__':
+ app.run(port='8080', host='0.0.0.0')
